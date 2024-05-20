2e5bcf183aaca9f114c021c2b5633f7b06667d7f
William-Benoît Lambert <1735971@etu.cegep-lanaudiere.qc.ca>
2024-05-20 10:21:20 -0400

    [DELETE] - Infrastructure | Suppression des platforms qui ne seront pas utilisé et suppression du RCL WebUI car trop compliqué à mettre en place pour le peu de valeur ajouté que cela apporte

--- QueueControl/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace QueueControl;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
